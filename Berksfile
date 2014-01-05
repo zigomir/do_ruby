56f5126eef191804bb792724fbd39ecd8c0abab1
zigomir <zigomir@gmail.com>
2014-01-05 15:55:12 +0100

    dependencies needs to be specified in metadata.rb

--- Berksfile
+++ Berksfile
@@ -1,9 +1,3 @@
 source 'http://api.berkshelf.com'
 
 metadata
-
-cookbook 'user'
-cookbook 'apt'
-cookbook 'git'
-cookbook 'build-essential'
-cookbook 'rbenv'
--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,9 @@ license          'MIT'
 description      'Digital Ocean box for Ruby/Rails projects'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
+
+depends 'user', '~> 0.3.0'
+depends 'apt', '~> 2.3.4'
+depends 'git', '~> 2.9.0'
+depends 'build-essential', '~> 1.4.2'
+depends 'rbenv', '~> 1.7.1'
